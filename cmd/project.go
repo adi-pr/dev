e7ec64e8c40c7611f17829b0d01bba4a1f08d2eb
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2026-09-21 08:43:01 -0400

    feat: project statuses

--- cmd/project.go
+++ cmd/project.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/adi-pr/dev/internal/config"
 	"github.com/adi-pr/dev/internal/project"
@@ -12,6 +13,7 @@ import (
 )
 
 var projectJSON bool
+var projectStatusJSON bool
 
 var projectCmd = &cobra.Command{
 	Use:   "project",
@@ -90,16 +92,137 @@ var projectOpenCmd = &cobra.Command{
 	},
 }
 
+var projectStatusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Show the status of known projects",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.Load()
+		if err != nil {
+			return err
+		}
+
+		projects, err := project.Discover(cfg.ProjectRoots)
+		if err != nil {
+			return err
+		}
+
+		statuses := make([]project.Status, 0, len(projects))
+
+		for _, p := range projects {
+			statuses = append(statuses, project.GetStatus(p))
+		}
+
+		if projectStatusJSON {
+			encoder := json.NewEncoder(os.Stdout)
+			encoder.SetIndent("", "  ")
+
+			return encoder.Encode(statuses)
+		}
+
+		fmt.Printf(
+			"%-24s %-20s %-10s %s\n",
+			"PROJECT",
+			"BRANCH",
+			"STATE",
+			"LAST COMMIT",
+		)
+
+		for _, status := range statuses {
+			if !status.Git {
+				fmt.Printf(
+					"%-24s %-20s %-10s %s\n",
+					status.Name,
+					"-",
+					"-",
+					"-",
+				)
+
+				continue
+			}
+
+			state := "clean"
+
+			if status.Dirty {
+				state = "dirty"
+			}
+
+			lastCommit := "-"
+
+			if status.LastCommit != nil {
+				lastCommit = formatRelativeTime(*status.LastCommit)
+			}
+
+			fmt.Printf(
+				"%-24s %-20s %-10s %s\n",
+				status.Name,
+				status.Branch,
+				state,
+				lastCommit,
+			)
+		}
+
+		return nil
+	},
+}
+
+func formatRelativeTime(t time.Time) string {
+	duration := time.Since(t)
+
+	switch {
+	case duration < time.Minute:
+		return "just now"
+
+	case duration < time.Hour:
+		minutes := int(duration.Minutes())
+
+		if minutes == 1 {
+			return "1 minute ago"
+		}
+
+		return fmt.Sprintf("%d minutes ago", minutes)
+
+	case duration < 24*time.Hour:
+		hours := int(duration.Hours())
+
+		if hours == 1 {
+			return "1 hour ago"
+		}
+
+		return fmt.Sprintf("%d hours ago", hours)
+
+	case duration < 7*24*time.Hour:
+		days := int(duration.Hours() / 24)
+
+		if days == 1 {
+			return "1 day ago"
+		}
+
+		return fmt.Sprintf("%d days ago", days)
+
+	default:
+		return t.Format("2006-01-02")
+	}
+}
+
 func init() {
 	rootCmd.AddCommand(projectCmd)
 
 	projectCmd.AddCommand(projectListCmd)
 	projectCmd.AddCommand(projectOpenCmd)
+	projectCmd.AddCommand(projectStatusCmd)
 
 	projectListCmd.Flags().BoolVar(
 		&projectJSON,
 		"json",
 		false,
 		"output as JSON",
+	)
+
+	projectStatusCmd.Flags().BoolVar(
+		&projectStatusJSON,
+		"json",
+		false,
+		"output as json",
 	)
 }
--- internal/project/project.go
+++ internal/project/project.go
@@ -3,15 +3,26 @@ package project
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"sort"
 	"strings"
+	"time"
 )
 
 type Project struct {
 	Name string `json:"name"`
 	Path string `json:"path"`
 	Git  bool   `json:"git"`
+}
+
+type Status struct {
+	Name       string     `json:"name"`
+	Path       string     `json:"path"`
+	Git        bool       `json:"git"`
+	Branch     string     `json:"branch,omitempty"`
+	Dirty      bool       `json:"dirty"`
+	LastCommit *time.Time `json:"last_commit,omitempty"`
 }
 
 func Discover(roots []string) ([]Project, error) {
@@ -57,4 +68,44 @@ func Find(projects []Project, name string) (Project, error) {
 	}
 
 	return Project{}, fmt.Errorf("project %q not found", name)
+}
+
+func GetStatus(p Project) Status {
+	status := Status{
+		Name: p.Name,
+		Path: p.Path,
+		Git:  p.Git,
+	}
+
+	if !p.Git {
+		return status
+	}
+
+	status.Branch = gitOutput(p.Path, "branch", "--show-current")
+
+	dirty := gitOutput(p.Path, "status", "--porcelain")
+	status.Dirty = dirty != ""
+
+	lastCommit := gitOutput(p.Path, "log", "-1", "--format=%cI")
+
+	if lastCommit != "" {
+		t, err := time.Parse(time.RFC3339, lastCommit)
+		if err == nil {
+			status.LastCommit = &t
+		}
+	}
+
+	return status
+}
+
+func gitOutput(path string, args ...string) string {
+	cmd := exec.Command("git", args...)
+	cmd.Dir = path
+
+	output, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+
+	return strings.TrimSpace(string(output))
 }
